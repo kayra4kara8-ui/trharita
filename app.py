9f21f6e20abadd2fa98c90e9bd5508b76fbbf60a
kayra4kara8-ui <kayra4kara8@gmail.com>
2026-01-24 21:54:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -520,10 +520,10 @@ CITY_NORMALIZE_CLEAN = {
     'SİNOP': 'Sinop',
     'SIVAS': 'Sivas',
     'SİVAS': 'Sivas',
-    'SANLIURFA": "Sanliurfa',
+    'SANLIURFA': 'Sanliurfa',
     'ŞANLIURFA': 'Sanliurfa',
-    'SIRNAK": "Sirnak',
-    'ŞIRNAK": "Sirnak',
+    'SIRNAK': 'Sirnak',
+    'ŞIRNAK': 'Sirnak',
     'TEKIRDAG': 'Tekirdag',
     'TEKİRDAĞ': 'Tekirdag',
     'TOKAT': 'Tokat',
@@ -5014,3 +5014,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
