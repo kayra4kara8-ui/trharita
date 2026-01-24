120a3a91a971e9b3617b40e4cfff24a069de20be
kayra4kara8-ui <kayra4kara8@gmail.com>
2026-01-24 21:18:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -534,7 +534,7 @@ CITY_NORMALIZE_CLEAN = {
     'VAN': 'Van',
     'YALOVA': 'Yalova',
     'YOZGAT': 'Yozgat',
-    'ZONGULDAK': 'Zonguldak",
+    'ZONGULDAK': 'Zonguldak',
     'ZONGULDAK': 'Zonguldak",
     'ARDAHAN': 'Ardahan',
     'AKSARAY': 'Aksaray',
@@ -4942,3 +4942,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
